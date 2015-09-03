c7123d20a865aaac2e451cef1ed7ee0dd5ee15a9
Maik Kempe <mkempe@bitaculous.com>
2015-09-03 14:52:29 +0200

    [➠] Refactoring and improved specs.

--- lib/mongoid/uuid.rb
+++ lib/mongoid/uuid.rb
@@ -2,14 +2,27 @@ require 'bson'
 require 'mongoid'
 require 'uuid'
 
-require 'mongoid/uuid/field'
 require 'mongoid/uuid/validator'
 
 module Mongoid
   module UUID #:nodoc:
     extend ActiveSupport::Concern
 
-    include Field
+    included do
+      # === Fields ===
+
+      field :uuid, type: BSON::Binary, default: -> { BSON::Binary.new ::UUID.new.generate, :uuid }, pre_processed: true
+
+      attr_readonly :uuid
+
+      # === Validations ===
+
+      validates :uuid, uniqueness: true, uuid: true
+
+      # === Indexes ===
+
+      index({ uuid: 1 }, unique: true, name: 'uuid_index')
+    end
 
     def self.validate(uuid)
       uuid.class.to_s == 'BSON::Binary' && uuid.type == :uuid && ::UUID.validate(uuid.data)
--- spec/mongoid/uuid_spec.rb
+++ spec/mongoid/uuid_spec.rb
@@ -1,4 +1,20 @@
-RSpec.describe Mongoid::UUID do
+RSpec.describe Mongoid::UUID, type: :model do
+  describe 'concerns' do
+    subject { Dummy.new }
+
+    describe 'fields' do
+      it { is_expected.to have_field(:uuid).of_type BSON::Binary }
+    end
+
+    describe 'validations' do
+      it { is_expected.to validate_uniqueness_of :uuid }
+    end
+
+    describe 'indexes' do
+      it { is_expected.to have_index_for(uuid: 1).with_options name: 'uuid_index' }
+    end
+  end
+
   describe 'creation' do
     it 'generates an UUID' do
       dummy = Dummy.create! name: 'Dummy'
@@ -25,6 +41,24 @@ RSpec.describe Mongoid::UUID do
 
       expect(dummy.valid?).to be false
       expect { dummy.save! }.to raise_error Mongoid::Errors::Validations
+    end
+  end
+
+  describe 'UUID is a read only attribute' do
+    context 'update' do
+      it 'raises an error' do
+        expect {
+          Dummy.create!(name: 'Dummy').update_attribute :uuid, 'baz-baz1'
+        }.to raise_error Mongoid::Errors::ReadonlyAttribute
+      end
+    end
+
+    context 'remove' do
+      it 'raises an error' do
+        expect {
+          Dummy.create!(name: 'Dummy').remove_attribute :uuid
+        }.to raise_error Mongoid::Errors::ReadonlyAttribute
+      end
     end
   end
 end
