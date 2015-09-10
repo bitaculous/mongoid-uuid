b0c005572d66dd2e19187e853144bd1ca34095ae
Maik Kempe <mkempe@bitaculous.com>
2015-09-10 22:10:44 +0200

    [➠] Make RuboCop happy.

--- lib/mongoid/uuid/rspec/shared/examples/read_only_attribute.rb
+++ lib/mongoid/uuid/rspec/shared/examples/read_only_attribute.rb
@@ -1,15 +1,7 @@
 RSpec.shared_examples 'Mongoid::UUID::ReadOnlyAttribute' do |object|
-  describe 'UUID is a read only attribute' do
-    context 'update' do
-      it 'raises an error' do
-        expect { object.update_attribute :uuid, 'baz-baz1' }.to raise_error Mongoid::Errors::ReadonlyAttribute
-      end
-    end
+  it 'raises an error if the UUID is updated or deleted' do
+    expect { object.update_attribute :uuid, 'baz-baz1' }.to raise_error Mongoid::Errors::ReadonlyAttribute
 
-    context 'remove' do
-      it 'raises an error' do
-        expect { object.remove_attribute :uuid }.to raise_error Mongoid::Errors::ReadonlyAttribute
-      end
-    end
+    expect { object.remove_attribute :uuid }.to raise_error Mongoid::Errors::ReadonlyAttribute
   end
 end
