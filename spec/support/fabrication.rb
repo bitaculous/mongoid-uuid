1b2a62bc19928bc25afbe0c2a5d2bfb4cf2c75e5
Maik Kempe <mkempe@bitaculous.com>
2015-09-21 23:11:58 +0200

    [➠] Moved fabricators under `spec/resources`.

--- spec/support/fabrication.rb
+++ spec/support/fabrication.rb
@@ -1,6 +1,6 @@
 require 'fabrication'
 
 Fabrication.configure do |config|
-  config.fabricator_path = 'spec/fabricators'
+  config.fabricator_path = 'spec/resources/fabricators'
   config.sequence_start  = 0
 end
